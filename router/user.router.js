a47bf7984ef9955ecda199336403b920b5c7a976
Richard Castro <richardcastro@MacBook-Air-de-Richard.local>
2017-10-13 19:32:11 -0500

    V0.9

--- router/user.router.js
+++ router/user.router.js
@@ -3,7 +3,7 @@ const handler = require('../utils/handler');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/unidad02', {
+mongoose.connect('mongodb://localhost:27017/unidad2', {
   useMongoClient: true
 });
 
@@ -17,11 +17,56 @@ module.exports = () => {
         .exec(handler.handleMany.bind(null, 'users', res));
     });
 
+    
+
     router.get('/:id', (req, res) => {
         const id = req.params.id;
         User.find({_id:id})
         .sort()
-        .exec(handler.handleMany.bind(null, 'users', res));
+        .exec(handler.handleOne.bind(null, 'users', res));
+    });
+
+    /*--inserciones--*/
+
+    router.post('/', (req, res) => {
+        const usuario = req.body;
+
+        User.create(usuario)
+        .then(
+            function(data){
+                console.log(data);
+                res.jason(data);
+            }
+        )
+        .catch(
+            function(err){
+                console.log(err);
+                res.status(400);
+                rest.jason({Error:err});
+
+            }
+        )
+        
+    });
+
+   
+
+    /*--Eliminaciones--*/
+
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+
+        User.remove({_id:id},function(err,data){
+            if(err){
+                console.log(err);
+                res.status(400);
+                res.json({error:err});
+            }else{
+                res.json({msj:"todo estuvo bien"});
+            }
+        
+        });
+        
     });
 
     return router;
